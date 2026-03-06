549dcea22bc3247d0fb7b16dac1d2b4a8e0b1230
Scott Brabson <scott.brabson@coalesce.io>
2026-03-06 12:51:12 -0700

    CD-13124 add note about suffix options

--- src/api/clone.js
+++ src/api/clone.js
@@ -26,7 +26,7 @@ import { join } from '../utils/join.js'
  * @param {boolean} [args.noCheckout = false] - If true, clone will only fetch the repo, not check out a branch. Skipping checkout can save a lot of time normally spent writing files to disk.
  * @param {boolean} [args.noTags = false] - By default clone will fetch all tags. `noTags` disables that behavior.
  * @param {string} [args.remote = 'origin'] - What to name the remote that is created.
- * @param {string} [args.filter] - Partial clone filter spec sent to the server during fetch. Limits which objects the server includes in the packfile. Common values: `'blob:limit=2097152'` (exclude blobs larger than 2MB), `'blob:none'` (exclude all blobs), `'tree:0'` (exclude all trees). Requires server support for the `filter` capability. Filtered-out objects will be skipped during checkout.
+ * @param {string} [args.filter] - Partial clone filter spec sent to the server during fetch. Limits which objects the server includes in the packfile. Common values: `'blob:limit=2097152'` (exclude blobs larger than 2MB), `'blob:none'` (exclude all blobs), `'tree:0'` (exclude all trees). Values are in bytes by default; `k`, `m`, and `g` suffixes are also supported (e.g. `'blob:limit=2m'`). Requires server support for the `filter` capability. Filtered-out objects will be skipped during checkout.
  * @param {number} [args.depth] - Integer. Determines how much of the git repository's history to retrieve
  * @param {Date} [args.since] - Only fetch commits created after the given date. Mutually exclusive with `depth`.
  * @param {string[]} [args.exclude = []] - A list of branches or tags. Instructs the remote server not to send us any commits reachable from these refs.
